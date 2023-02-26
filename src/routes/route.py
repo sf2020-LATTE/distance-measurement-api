ffd2bd70e62b9e6682380804eda88ad7c22d6f05
sf2020-LATTE <second.s.fujii@gmail.com>
2023-02-26 16:00:40 +0900

    レスポンスのスキーマ策定、クエリパラメータのバリデーション追加

--- src/routes/route.py
+++ src/routes/route.py
@@ -1,9 +1,11 @@
 from fastapi import (
-    APIRouter
+    APIRouter,
+    Query
 )
 from decimal import Decimal
 
 from app.cruds.distance import calculate_distance
+import app.schemas.schema as schema_model
 
 router = APIRouter()
 
@@ -13,7 +15,12 @@ async def root():
     return {"message": "Hello World"}
 
 
-@router.get("/distance/")
-async def get_distance(start_longitude : Decimal = 0, start_latitude: Decimal = 0, end_longitude: Decimal = 0, end_latitude: Decimal = 0):
+@router.get("/distance/", response_model=schema_model.Calculate_result)
+async def get_distance(
+    start_longitude : Decimal = Query(ge=0, le=180),
+    start_latitude: Decimal = Query(ge=0, le=180),
+    end_longitude: Decimal = Query(ge=0, le=180),
+    end_latitude: Decimal = Query(ge=0, le=180)
+):
     calculate_distance_result = calculate_distance(start_longitude, start_latitude, end_longitude, end_latitude)
-    return calculate_distance_result
+    return schema_model.Calculate_result(distance=calculate_distance_result)
